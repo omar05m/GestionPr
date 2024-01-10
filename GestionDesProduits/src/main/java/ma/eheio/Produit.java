008d1923722540bdaaf0b22f6c12439d5f8038dd
omar05m <omarkof99@gmail.com>
2024-01-10 17:25:46 +0100

    l'ajout de projet

---
+++ GestionDesProduits/src/main/java/ma/eheio/Produit.java
@@ -0,0 +1,45 @@
+package ma.eheio;
+
+
+public class Produit {
+    private long id;
+    private String nom;
+    private double prix;
+    private int qte;
+
+    public Produit(long id, String nom, double prix, int qte) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.qte = qte;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public double getPrix() {
+        return prix;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public int getQte() {
+        return qte;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public void setQte(int qte) {
+        this.qte = qte;
+    }
+}
---
+++ GestionDesProduits/src/main/java/ma/eheio/ProduitService.java
@@ -0,0 +1,12 @@
+package ma.eheio;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ProduitService {
+    Map<Long,Produit> produits = new HashMap<Long, Produit>();
+
+
+}
