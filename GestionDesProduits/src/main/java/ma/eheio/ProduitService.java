770c7ca2638f9929f56f2a421f9f39ebb9afd6c7
Khalil Mohammed Berkani <khalilikafo@gmail.com>
2024-01-11 21:43:24 +0100

    ajout de la method supprimerProduit

--- GestionDesProduits/src/main/java/ma/eheio/ProduitService.java
+++ GestionDesProduits/src/main/java/ma/eheio/ProduitService.java
@@ -8,5 +8,13 @@ import java.util.Set;
 public class ProduitService {
     Map<Long,Produit> produits = new HashMap<Long, Produit>();
 
-
+    //supprimer un produits
+    public void supprimerProduit(Produit produit) {
+        if (produits.containsValue(produit)) {
+            produits.values().remove(produit);
+            System.out.println("produit supprime");
+        } else {
+            System.out.println("produit existe pas");
+        }
+    }
 }
